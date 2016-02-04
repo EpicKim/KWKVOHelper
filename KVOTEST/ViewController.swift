f12636dfae50763851a16177b9f9927584715d1b
Kim <wangk@172-15-12-41.lightspeed.sntcca.sbcglobal.net>
2016-02-04 10:20:19 +0800

    init

---
+++ KVOTEST/ViewController.swift
@@ -0,0 +1,41 @@
+//
+//  ViewController.swift
+//  KVOTEST
+//
+//  Created by wangk on 16/2/2.
+//  Copyright © 2016年 wangk. All rights reserved.
+//
+
+import UIKit
+
+class SSViewController: UIViewController {
+    dynamic var test = 1
+    dynamic var two = 2
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+//        self.
+        self.observe("test", closure: { () -> Void in
+            print(self.test)
+        })
+
+        self.observe("two", closure: { () -> Void in
+            print(self.two)
+        })
+        test = 2
+        test = 34
+        test = 444
+        
+        two = 12141
+        two = 32414
+        two = 414141
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ KWKVOHelper.swift
@@ -0,0 +1,54 @@
+//
+//  KWKVOHelper.swift
+//  Kvo-Example
+//
+//  Created by wangk on 16/2/2.
+//  Copyright © 2016年 EVER SENSE, INC. All rights reserved.
+//
+
+import Foundation
+
+extension NSObject {
+    func observe(keypath: String, closure:()->Void) {
+        self.addObserver(KWKVOMananger.sharedInstance, forKeyPath: keypath, options: .New, context: nil)
+        KWKVOMananger.sharedInstance.add(keypath, closure: closure, object: self.classForCoder)
+    }
+}
+
+public class KWKVOObject : NSObject {
+    public var closure:()->Void = { }
+    public var objectClass:AnyClass?
+    public var keyPath:String?
+}
+
+public class KWKVOMananger : NSObject  {
+    class var sharedInstance:KWKVOMananger {
+        struct Static {
+            static var onceToken : dispatch_once_t = 0
+            static var instance : KWKVOMananger? = nil
+        }
+        dispatch_once(&Static.onceToken) {
+            Static.instance = KWKVOMananger()
+        }
+        return Static.instance!
+    }
+    
+    var kvoObjects = NSMutableArray()
+    
+    public func add(keyPath: String, closure:()->Void, object:AnyClass) {
+        let newObject = KWKVOObject()
+        newObject.closure = closure
+        newObject.keyPath = keyPath
+        newObject.objectClass = object
+        self.kvoObjects.addObject(newObject)
+    }
+    
+    override public func observeValueForKeyPath(keyPath: String?, ofObject object: AnyObject?, change: [String : AnyObject]?, context: UnsafeMutablePointer<Void>) {
+//        let newString = change![NSKeyValueChangeNewKey]
+        let predicate = NSPredicate(format: "keyPath=%@", keyPath!)
+        let filteredArray = self.kvoObjects.filteredArrayUsingPredicate(predicate)
+        
+        let kvoObject = filteredArray.first as! KWKVOObject
+        kvoObject.closure()
+    }
+}
